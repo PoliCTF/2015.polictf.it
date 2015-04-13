30a12fad1b1d31881dbefc3e8bf616d24a6f1200
ocean1 <ocean.kuzuri@gmail.com>
2015-04-13 20:48:41 +0200

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd build
 git init
 git add -A
 git commit -m "deploy static website"
-git push -u git@github.com:TowerofHanoi/polictf.git master:gh-pages --force
+git push -u git@github.com:PoliCTF/polictf.github.io master:master --force
 
