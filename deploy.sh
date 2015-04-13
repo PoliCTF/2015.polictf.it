ae1f6a4cbe7370bf037baf6defc770d308535f6b
ocean1 <ocean.kuzuri@gmail.com>
2015-04-13 22:28:08 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
-workon polictf
-rm -rf build
-clay build
-cd build
-git init
-git add -A
-git commit -m "deploy static website"
-git push -u git@github.com:PoliCTF/polictf.github.io master:master --force
-
+#!/bin/sh
+which clay
+if [ $? -eq 0 ]; then
+    rm -rf build
+    clay build
+    cd build
+    git init
+    git add -A
+    git commit -m "deploy static website"
+    git push -u git@github.com:PoliCTF/polictf.github.io master:master --force
+fi
