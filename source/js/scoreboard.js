f63c1e33718c71966684ceeb61605fafc10b9709
Alessandro <cubo@trallallino.it>
2015-05-21 16:03:56 +0200

    creata scoreboard dinamica

--- source/js/scoreboard.js
+++ source/js/scoreboard.js
@@ -5,3 +5,18 @@ function setVisibility(id){
   }
   $("#"+id).fadeIn("slow");
 }
+
+function getScores(){
+  $.ajax({
+    type:"GET",
+    url:"http://scoreboard.polictf.local.necst.it/scoreboard/common/status",
+    success: function(data, status){
+      array_scores = data.scores;
+      for(i = 0; i < array_scores.length; i++){
+	string = "<ul><li>" + i + "</li><li>" + array_scores[i].name + "</li><li class=\"image-points\">" + array_scores[i].points + "</li><li class=\"image-levels\">" + "1/3" + "</li></ul>"; 
+	$("#scores").append(string);
+      }
+    },
+    error: function() { alert('ummh..'); }
+  });
+}
