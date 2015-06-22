2e535d47018eff7c2175b8d2cb6660414136e8c2
Jiang Wu <info@jiangwu.eu>
2015-06-22 12:41:16 +0200

    Some changes... Not finished yet.

--- source/js/scoreboard.js
+++ source/js/scoreboard.js
@@ -330,7 +330,7 @@ function compare_unixtime(a,b) {
 }
 
 
-
+/*
 function getChallengesList(){
 
 	$.blockUI("<h3>Loading...</h3>"); 
@@ -357,4 +357,4 @@ function getChallengesList(){
     	$("#result").fadeIn("slow");
     }
   });
-}
+}*/
